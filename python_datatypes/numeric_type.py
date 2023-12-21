1f1dba6eef90b9db455c1b09b54621f2dbb0be05
Arshvir <avarshvir@gmail.com>
2023-12-21 19:33:47 +0530

    commit

---
+++ python_datatypes/numeric_type.py
@@ -0,0 +1,9 @@
+a = 5                 #this is integer
+b = 1.46              #this is float
+c = True              #this is boolean
+d = 1+2j              #this is complex
+print(a,'\n',b,'\n',c,'\n',d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ python_datatypes/text_type.py
@@ -0,0 +1,7 @@
+e = "namaste"
+f = 'hello'
+g = '''hi'''
+print(e,f,g)
+print(type(e))
+print(type(f))
+print(type(g))
