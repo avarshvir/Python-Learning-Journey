11e3169563d3510c11365dc3e38f91a44e59e819
Arshvir <avarshvir@gmail.com>
2024-01-17 22:13:33 +0530

    Create series_12_1.py

---
+++ python_12_pandas/python_12_1_series/series_12_1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+data = [1,2,3,4]
+print(data)
+ps = pd.Series(data)
+print(ps)
+
+#label
+print("label...")
+print(ps[1])
+
+#index
+print("index...")
+ind = pd.Series(data,index = ['x','y','a',1])
+print(ind)
+
+#Key/Value Objects as Series
+#The keys of the dictionary become the labels.
+print("Key/Value Objects as Series...")
+calories = {"day1": 421, "day2": 380, "day3": 390}
+myvar = pd.Series(calories)
+print(myvar)
+print(myvar["day1"])
