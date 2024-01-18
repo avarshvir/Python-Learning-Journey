8810f52423ab0d30741b066a0b5b65c8d5c83c02
Arshvir <avarshvir@gmail.com>
2024-01-18 19:31:29 +0530

    Update dataframe_12_1.py

--- python_12_pandas/python_12_2_dataframes/dataframe_12_1.py
+++ python_12_pandas/python_12_2_dataframes/dataframe_12_1.py
@@ -1,1 +1,17 @@
-#coming soon
+import pandas as pd
+data = {
+    "Animal":["Lion","Tiger","Leopard","Jaguar","Cheetah","Black Panther","Puma","Snow Leopard"],
+    "BiteForce" : [600,1000,600,1500,400,1500,450,600]
+}
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+print(df)
+
+#refer to the row index:
+print("row index...")
+print(df.loc[0])
+print(df.loc[[1,6]])
+
+#index
+print("index...")
+print(pd.DataFrame(data, index=['a','b','c','d','e','f','g','h']))
