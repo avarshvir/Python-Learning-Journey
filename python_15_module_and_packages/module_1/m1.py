6f4dedc9e1ba2904adb93503a19b47612b3f6d49
avarshvir <avarshvir@gmail.com>
2025-02-20 17:21:07 +0530

    module

---
+++ python_15_module_and_packages/module_1/m1.py
@@ -0,0 +1,11 @@
+print("This is Module 1")
+
+def mod_1():
+    print("This is mod_1")
+
+def mod_2(a):
+    print("This is mod_2 with: ",a)
+
+def mod_3(a):
+    print("This is mod_3")
+    return a
