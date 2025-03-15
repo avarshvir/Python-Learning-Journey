a98e41748f9467294cc867d6e5a6c5a42808e911
avarshvir <avarshvir@gmail.com>
2025-03-15 22:58:56 +0530

    python dictionary

--- python_8_data_structures/python_dictionary/python_dictionary.py
+++ python_8_data_structures/python_dictionary/python_dictionary.py
@@ -6,29 +6,42 @@ a_dict = {
 }
 print(a_dict)
 
+print("---------------------------------")
+
 #accessing
 x = a_dict['year']
 print(x)
 '''y = a_dict[3]
 print(y)'''
+print("---------------------------------")
+
 
 #accessing by inbuilt methods
 #get method
 print(a_dict.get('name'))
+print("---------------------------------")
+
 #keys method
 print(a_dict.keys())
+print("---------------------------------")
+
 #values method
 print(a_dict.values())
+print("---------------------------------")
+
 #items method
 print(a_dict.items())
+print("---------------------------------")
 
 #modifying or changing values
 a_dict['version'] = 3.1
 print(a_dict)
+print("---------------------------------")
 
 #adding new item or key pair values
 a_dict['name of DS'] = 'tuple'
 print(a_dict)
+print("---------------------------------")
 
 #removing items
 a_dict.pop('name of DS')
@@ -37,14 +50,19 @@ print(a_dict)
 #del a_dict
 #print(a_dict)
 #print(a_dict.clear())
+print("---------------------------------")
 
 #looping in dictionary
 for i in a_dict:
     print(a_dict[i])
+print("---------------------------------")
+
 for j in a_dict:
     print(j)
+print("---------------------------------")
 
 #copy dictionary
 b_dict = a_dict.copy()
 print(b_dict)
+print("---------------------------------")
 
